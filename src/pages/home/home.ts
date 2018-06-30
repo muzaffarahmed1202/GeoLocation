1286f052eed18aba4706f46f268d95a69ff73111
pacewisdom <muzaffar.a@pacewisdom.com>
2018-06-30 12:50:03 +0530

    geo location

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
-import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Component, OnInit } from '@angular/core';
+
+
+declare var google:any;
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+selector: 'page-home',
+templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
 
-  }
+map: any;
+markers = [];
+
+constructor(public navCtrl: NavController) {
+}
+
+ngOnInit() {
+this.initMap();
+}
+
+private initMap() {
+var point = {lat: 13.038039, lng: 80.21597};
+let divMap = (<HTMLInputElement>document.getElementById('map'));
+this.map = new google.maps.Map(divMap, {
+center: point,
+zoom: 15,
+disableDefaultUI: true,
+draggable: false,
+zoomControl: true
+});
+
+this.createMapMarker(point);
+}
+
+private createMapMarker(place:any):void {
+var marker = new google.maps.Marker({
+map: this.map,
+position: place
+});
+this.markers.push(marker);
+}
+
 
 }
